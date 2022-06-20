b61e3725edba12feb8ffa04a2d7d5c89f67c48ee
Yves <yxn16a@acu.edu>
2022-06-20 12:10:04 +0200

    hellojs

---
+++ nodejstnote1.js
@@ -0,0 +1,22 @@
+// how did node was born:
+// 1995 was born, 1996 netscape created livewire  and this was the beginning of 
+// of the server side java scrip 
+// 2008 chrome was created and v8 engine was created
+// this made javascript to run faster and v8 
+// node js was created in 2009
+// node js run javascript outside the browser by Ryan dhal(the creater of node js)
+// it was built on v8 engine 
+// node is javascrip runtime not a frame work
+// runtime: is environment where javascript run
+// libuv:is a c library that work out with io
+// Ryan combined libuv and v8 engine
+// window object gives us the tools that we can 
+// chrome comes the java script engine
+// running javascript without engine
+// what happens by running node in the terminal: 
+// - REPL(READEVALPRINT AND LOOP) this happens when we run node in the terminal with no argu
+// ment for example we can run javascript code
+// REPL: our node will read, write, pring and loop
+// read:breaking the code into peaces and perse them(or evaluate them by v8 and the result
+// will be printed on the screen) 
+// REPL IS GOOD for testing not for saving the code for a long period of time
