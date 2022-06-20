77eb8ce6f09159314ea320e513aed984bb0f3368
Yves <yxn16a@acu.edu>
2022-06-20 15:56:01 +0200

    nodejsevents

---
+++ events.js
@@ -0,0 +1,35 @@
+const EventEmitter = require('events'); 
+const celebrity = new EventEmitter(); 
+// we have an observer to the one to the celeberity
+celebrity.on('race', (result) => { 
+    if (result === 'win') { 
+       console.log('Congratulations you are the best');   
+    }
+   
+})
+// what is happening above is that when eventemmiter realease an event such as race win
+// the observer or the listner which is the the function that prints the on the console 
+// will print congratulations you are the best. function is a listner and at the same time a 
+// call back function 
+
+// observe 2 may be a competitor to the race winner and they may say boo I would have won that 
+// race
+celebrity.on('race', (result) => { 
+    if (result === 'lost') {
+       console.log('Boo, I would have won that race loser...');   
+    }
+})
+// code = 0 when successful, 
+process.on('exit', (code) => { 
+    console.log('process exist event with code', code); 
+})
+
+// how do we trigger those events we call a call emit function 
+// we can trigger the events by calling a emit function on the celebrity function 
+celebrity.emit('race', 'win'); 
+celebrity.emit('race', 'lost'); 
+// using this model we can use this model to create celeblicy and listner
+//Process is an instance of event emitter
+// process has on function to react to events 
+// the process can respond to the exit event by printing the process exit event with code
+
--- hello.js
+++ hello.js
@@ -42,5 +42,88 @@ if (mission === 'learn') {
 // libforder: has javascript section of node
 // src:contain c++ files
 //.cc is the same ast 
-// LibUV SOURCE CODE
-//
+// LibUV SOURCE CODE: has many bindings for different languages such as python, Ruby
+// and other laguage
+//mac os, and lunix  is a decendent of unix (fun fact); 
+//libuv used c instead of c++
+// libuv calls function using unit 
+//SYNC: go through one by one 
+//ASYNC: takes more tasks while doing other tasks
+//ASYNC: Node is good at this process
+// Node is asyc
+// Call back functions: are functions that are called when certan activity is done
+// call backs are 
+//NON BLOCKING FUNCTION
+// Blocking code work sysnchroniously
+//setTime excute in the back groud
+// node asynchronious and this helps with the blockig functio
+
+// Is java script sync or Async: java script is sync excute line by like
+// javascript can be manipulated to be async
+// is node js multithreaded: one function can be run by two threads
+// threads lives in the code and under the processes
+// threads are independent of each other
+//(look the locks and threads in java and deadslock)
+// threads: Java scripts is a single threaded
+// nodes:runs java script async
+// in node we have one single threads that runs libuv, and v8 engine
+//lib uv handles file system and netword
+// EVENT LOOP
+// ASYN functions are put in the events Loop and then when complete the result is brought 
+// back
+// lib uv has a collection of threads and are set to take work as they come
+// there are 4 threads seating waiting(ASYNCH)
+// file system libuv send it to one of thread then event loop 
+// WHERE POSSIBLE LIB UV DOES USE OPERATING SYSTEM AND WE DO NOT USE THREADS
+// THIS ALLOWS TO RUN ASYNCHRONOUS I/O
+// NODE DOES NOT COMPLICATE LIFE BY USING FEW THREAD
+// EVENT loops: 
+// event loop allows to run node to as asynchronous 
+// it checks if node process should exit and if not then it process all the events sent to it 
+// this loop works when the node is running and the actual loop lives in lib uv and implemented in c
+// How does LOOP WORKS
+// it uses a queu like when the time is done for call 
+// when the time has passed then what happens is that the new call back is passed to the callback queue
+// HOW EVENTS PROCESSES EVENTS
+// there are several call back queue
+// there are difference queues that handles each phase of operations
+// EVENT LOOP PHASES: these phases have queues and thes phase are as follows: 
+// Timers
+// I/O callbacks
+// setImmediate
+// close callbacks
+// for example: 
+// a call for set timer goes to timers queue
+// TIMERS: 
+// setTimeout
+// setTimeInterval
+// setImmediate
+// all of those above time goes to timers phase
+// EVENT LOOP
+// setImmediate: is excuted immediatly but it does not excute immediary
+// when the loop start: TIMERS ARE CHECKED TO SEE IF THERE ARE TIMERS FUNCTION
+// THIS FUNCTION IS CALLED ticks
+//time functions then i/o callbacks then setImmediate(called this because after i/o) then close
+// callbacks is called
+// COMPARING NODE, PHP, PYTHON(ALL ARE LIKE JAVASCRIPT)
+// NODE BECAME SO POPULAT BECAUSE OF NON BLOCKING 
+// ENGINE - LIBUV- THEN EVENT LOOPS SOME TIME THE LIBUV SEND SOME TO THE PROCESS DIRECT TO THE 
+// OPERATING SYSTEM
+// PHP: Create crazy threads for each crient when used with apatch
+// WHEN DO WE USED NODE: 
+// It is not good for machine learning because they are blocking
+// node is good at servers
+// email and authetication 
+// works well input and output
+// good for serving data for such as sending and receiving data
+// node is made for modern web
+// NODE is an vent driven
+// Design pattern: helps us to not invent a wheel
+// obsever design pattern 
+// a subject that notifies many observer
+// OBSERVER PATTERN IN NODE: 
+//EVENTS ARE NOTIFIED USING CALLBACK FUNCTIONS
+//EVENTS MODULES
+// LISTNERS ARE CALLED BACK FUNCTIONS
+//EVENTS EMMITTERS: calls require functions 
+// require allows us to use event module
