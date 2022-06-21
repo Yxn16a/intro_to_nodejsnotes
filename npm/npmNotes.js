0e573cd9ca3a5f2b454399c65755c73b9c7bcc17
Yves <yxn16a@acu.edu>
2022-06-21 14:05:54 +0200

    added somenotes  on the package

---
+++ npm/npmNotes.js
@@ -0,0 +1,16 @@
+// modules released by other developers can help us to develop faster
+// modules we can find those at npm website
+// npm is used to manage other packages
+// it create centralized code and we can look at in on place
+//  how do we create npm  packages
+// is collect of packages
+// to install axios for http 
+// when you do not care about about the description and athours in creating a new package
+// dependence has been added after axios installagion and this keeps the track of 
+// packages versions and 
+// if you want to can include start:"node fileto run when start is entered"
+// you add this start in the script of the package.json file
+// example will be like the following
+// "start" : "node https.js"
+// why did we need to initialize our packages
+// packages: means the packages that  
